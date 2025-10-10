7b5946ace09fd4492cd1c49ce91c1c3117e95590
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2025-10-10 08:38:54 +0700

    pertemuan ke-5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css/custom-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/CSS/custom-style.css') }}">
     <style>
         body {
-            font-family: 'Arial', sans-serif;
+            /* font-family: 'Arial', sans-serif; */
         }
 
         .navbar-brand {
