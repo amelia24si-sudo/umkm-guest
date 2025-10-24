a7e9c85e9cdb2e5387006ff5d790d9cb464e6082
amelia24si-sudo <amelia24si@mahasiswa.pcr.ac.id>
2025-10-24 10:54:46 +0700

    CRUD Users

--- app/Http/Controllers/UserControllers.php
+++ app/Http/Controllers/UserControllers.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserControllers extends Controller
 {
 
     public function index()
     {
-        return view('admin.user.index');
+        $data['dataUser'] = User::all();
+
+        return view('admin.user.index', $data);
     }
 
     public function create()
@@ -22,12 +25,12 @@ class UserControllers extends Controller
     {
         $data['name'] = $request->name;
         $data['email'] = $request->email;
-        $data['password'] = $request->password;
+        $data['password'] = Hash::make($request->password);
         // $data['password_confirmation'] = $request->password;
 
         User::create($data);
 
-        return redirect()->route('users.index')->with('success', 'Penambahan User Berhasil!');
+        return redirect()->route('user.index')->with('success', 'Penambahan User Berhasil!');
     }
 
     public function show(string $id)
@@ -37,7 +40,8 @@ class UserControllers extends Controller
 
     public function edit(string $id)
     {
-        //
+        $data['dataUser'] = User::findOrFail($id);
+        return view('admin.user.edit', $data);
     }
 
     public function update(Request $request, string $id)
@@ -47,6 +51,9 @@ class UserControllers extends Controller
 
     public function destroy(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user -> delete();
+        return redirect()->route('users.index')->with('success', 'Data berhasil dihapus');
     }
 }
